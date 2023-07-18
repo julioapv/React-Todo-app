00475635873ce3371e152eaa5a1311da87e235c7
Julio Peña <julio@pop-os.localdomain>
2023-07-18 18:26:39 -0400

    I created the first components

---
+++ src/TodoCounter.js
@@ -0,0 +1,9 @@
+function TodoCounter() {
+    return(
+        <h1>
+            You have completed 3 of 6 tasks
+        </h1>
+    )
+}
+
+export { TodoCounter };
---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem() {
+    return(
+      <li>
+        <span>V</span>
+        <p>Finish REACT course</p>
+        <span>X</span>
+      </li>
+    )
+}
+
+export { TodoItem };
